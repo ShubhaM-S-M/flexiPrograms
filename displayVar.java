b7f2e42493c859f1a336548ef35d3a5fee599a0f
Shubham Malesha <shubhammalesha@Shubhams-Mackbook-Air.local>
2026-01-20 09:52:29 +0530

    Updated Variable

--- displayVar.java
+++ displayVar.java
@@ -2,6 +2,10 @@ class displayVar
 {
 	public static void main(String[] args)
 	{
+		int x=5;
+		int y=6;
+		int a=1,b=2,c=3;
+		System.out.println(x+y);
 		String fname= "Shubham";
 		String lname= "Malesha";
 		String name= fname+ " "+lname;
