dafed99216e2e04bca8077e46d4f5a2af6deb201
Shubham Malesha <shubhammalesha@Shubhams-Mackbook-Air.local>
2026-01-20 09:48:31 +0530

    Printing string variable

---
+++ displayVar.java
@@ -0,0 +1,10 @@
+class displayVar
+{
+	public static void main(String[] args)
+	{
+		String fname= "Shubham";
+		String lname= "Malesha";
+		String name= fname+ " "+lname;
+		System.out.println(name);
+	}
+}
