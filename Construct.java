ee4096a50746c7c762684a31ede543802f76468e
Shubham Malesha <shubhammalesha@Shubhams-Mackbook-Air.local>
2026-01-23 12:01:33 +0530

    Constructor in java

--- Construct.java
+++ Construct.java
@@ -1,13 +1,21 @@
 public class Construct
 {	
 	int age;
+	int prn;
+	String name;
+	String sec;
 	Construct()
 	{
 		age=10;
+		prn=1;
+		name="Invalid";
+		sec="SY";
+		
 	}
 	public void disp()
 	{
-		System.out.println(age);
+		System.out.println("age = "+age+"\nname = "+name+"\nsec = "+sec+"\nprn = "+prn);
+		
 	}
 	public static void main(String[] args)
 	{
