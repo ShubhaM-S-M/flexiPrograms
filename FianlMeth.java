0b34e6bcb8d1d120cf6b10d96fd88bd6128570c8
Shubham Malesha <shubhammalesha@Shubhams-Mackbook-Air.local>
2026-01-30 12:00:47 +0530

    Final Method

---
+++ FianlMeth.java
@@ -0,0 +1,22 @@
+class Parent
+{
+	final void display()
+	{
+		System.out.println("Cannot be over ridden");
+	}
+}
+class Child extends Parent
+{
+	void diplay()
+	{
+		System.out.println("How did u do this?")
+	}
+}
+class FinalMeth
+{
+	public static void main(String[] args)
+	{
+		Child c = new Child();
+		c.display
+	}
+}
