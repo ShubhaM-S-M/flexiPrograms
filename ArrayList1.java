f45f3077ce1d5b46a478990dc7df728a3a754569
Shubham Malesha <shubhammalesha@Shubhams-Mackbook-Air.local>
2026-02-17 09:43:29 +0530

    Array List

---
+++ ArrayList1.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+class ArrayList1
+{
+	public static void main(String[] args)
+	{
+		ArrayList<String> fruits = new ArrayList<>();
+		fruits.add("Apple");
+		fruits.add("Banana");
+		fruits.add("Cherry");
+		System.out.println(fruits);	
+		fruits.remove("Banana");
+		System.out.println(fruits);
+		System.out.println("Size:"+fruits.size());
+		System.out.println("Contains Apple?: "+fruits.contains("Apple"));
+		System.out.println(fruits.get(0));
+	}
+}
